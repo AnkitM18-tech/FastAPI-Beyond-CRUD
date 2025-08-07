9d5e7db208c18470df14cee50f6800565234c6e3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-08 01:03:49 +0530

    dummy book data

---
+++ bookly/src/books/book_data.py
@@ -0,0 +1,29 @@
+books = [
+    {
+        "id": 1,
+        "title": "The Lord of the Rings",
+        "author": "J.R.R. Tolkien",
+        "publisher": "O'Reilly Media",
+        "published_date": "1954-07-29",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "The Hobbit",
+        "author": "J.R.R. Tolkien",
+        "publisher": "O'Reilly Media",
+        "published_date": "1937-09-21",
+        "page_count": 321,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The Silmarillion",
+        "author": "J.R.R. Tolkien",
+        "publisher": "O'Reilly Media",
+        "published_date": "1977-11-22",
+        "page_count": 654,
+        "language": "English",
+    },
+]
