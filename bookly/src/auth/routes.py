651a360c9f6291a560db8fd22bf8465224146c52
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-11 22:50:22 +0530

    sign up route

---
+++ bookly/src/auth/routes.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from sqlmodel.ext.asyncio.session import AsyncSession
+from .schemas import CreateUser, UserModel
+from .service import UserService
+from src.db.main import get_session
+
+auth_router = APIRouter()
+user_service = UserService()
+
+@auth_router.post("/signup", response_model=UserModel, status_code=status.HTTP_201_CREATED)
+async def create_user_account(user_data: CreateUser, session: AsyncSession = Depends(get_session)):
+    email = user_data.email
+    user_exists = await user_service.user_exists(email, session)
+
+    if user_exists:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+
+    new_user = await user_service.create_user(user_data, session)
+    return new_user
