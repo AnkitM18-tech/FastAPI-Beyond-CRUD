3a5647223ef6a1b1bf4ecae76e5bb9f947cc2841
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-11 22:50:06 +0530

    registering auth router

--- bookly/src/__init__.py
+++ bookly/src/__init__.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.books.routes import book_router
+from src.auth.routes import auth_router
 from src.db.main import init_db
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
-    print("Server is starting")
+    print("=============================== Server is starting ================================== ")
     await init_db()
     yield 
-    print("Server has been stopped")
+    print("=============================== Server has been stopped ============================= ")
 
 version = "v1"
 
@@ -20,3 +21,4 @@ app = FastAPI(
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
