bcd1c7f432b66e8dbd7c437316412f2c6f989123
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-11 14:00:06 +0530

    get_session sharing

--- bookly/src/db/main.py
+++ bookly/src/db/main.py
@@ -1,5 +1,7 @@
-from sqlmodel import create_engine, text, SQLModel
+from sqlmodel import create_engine, SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
 from src.config import Config
 
 engine = AsyncEngine(
@@ -10,3 +12,9 @@ async def init_db():
     async with engine.begin() as conn:
         from src.books.models import Book
         await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession: # pyright: ignore[reportInvalidTypeForm]
+    Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with Session() as session:
+        yield session
