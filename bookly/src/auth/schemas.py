d72969040fe7bd47db8d16b0be66edaf5a89a62e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-11 22:49:14 +0530

    create user and returned user schema

---
+++ bookly/src/auth/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+import uuid
+from datetime import datetime
+
+class CreateUser(BaseModel):
+    username: str = Field(max_length=20)
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+    first_name: str = Field(max_length=20)
+    last_name: str = Field(max_length=20)
+
+# can make use of existing User Model in models
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    password: str = Field(exclude=True)
+    first_name: str
+    last_name: str
+    created_at: datetime
+    updated_at: datetime 
+    is_verified: bool 
