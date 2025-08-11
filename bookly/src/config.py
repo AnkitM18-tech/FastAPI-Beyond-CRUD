5d7310bba201b4e3db3336130ff30d200c00ed00
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-12 01:28:20 +0530

    jwt specific env variables

--- bookly/src/config.py
+++ bookly/src/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URI : str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
