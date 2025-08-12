6a2d7ae753e7ed561c652c772173b1e0b651d68e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-12 16:03:24 +0530

    create new access token by using refresh token

--- bookly/src/auth/routes.py
+++ bookly/src/auth/routes.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
 from sqlmodel.ext.asyncio.session import AsyncSession
-from datetime import timedelta
+from datetime import timedelta, datetime
 from .schemas import CreateUser, UserModel, UserLogin
 from .service import UserService
 from .utils import create_access_token, verify_access_token, verify_password
 from src.db.main import get_session
+from .dependencies import RefreshTokenBearer
 
 auth_router = APIRouter()
 user_service = UserService()
@@ -49,3 +50,19 @@ async def login_user(user_data: UserLogin, session: AsyncSession = Depends(get_s
             }
         }
     )
+
+@auth_router.get("/refresh_token")
+async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer())):
+    expiry_timestamp = token_details["exp"]
+    if datetime.fromtimestamp(expiry_timestamp) > datetime.now():
+        new_access_token = create_access_token(
+            user_data=token_details["user"]
+        )
+
+        return JSONResponse(
+            content= {
+                "access_token": new_access_token
+            }
+        )
+    
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Refresh token has expired")
