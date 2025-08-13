ec7cc29378290334685257f1cca787f0e7e5a432
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-13 18:52:43 +0530

    route for user submitted books

--- bookly/src/books/routes.py
+++ bookly/src/books/routes.py
@@ -15,19 +15,25 @@ access_token_bearer = AccessTokenBearer()
 role_checker = Depends(RoleChecker(["admin", "user"]))
 
 @book_router.get("/", response_model= List[Books], dependencies=[role_checker])
-async def get_all_books(session: AsyncSession = Depends(get_session), user_details= Depends(access_token_bearer)):
+async def get_all_books(session: AsyncSession = Depends(get_session), token_details: dict = Depends(access_token_bearer)):
     books = await book_service.get_all_books(session)
     return books
 
+@book_router.get("/user/{user_id}", response_model= List[Books], dependencies=[role_checker])
+async def get_user_book_submissions(user_id: str,session: AsyncSession = Depends(get_session), token_details: dict = Depends(access_token_bearer)):
+    books = await book_service.get_user_books(user_id, session)
+    return books
+
 @book_router.post("/", status_code= status.HTTP_201_CREATED, response_model=Books, dependencies=[role_checker])
-async def publish_a_book(book: BookCreateModel, session: AsyncSession = Depends(get_session), user_details= Depends(access_token_bearer)) -> dict:
+async def publish_a_book(book: BookCreateModel, session: AsyncSession = Depends(get_session), token_details: dict = Depends(access_token_bearer)) -> dict:
     # new_book = book.model_dump()
-    new_book = await book_service.create_book(book, session)
+    user_id = token_details.get("user")["user_uid"]
+    new_book = await book_service.create_book(book, user_id, session)
     # books.append(new_book)
     return new_book
 
 @book_router.get("/{book_id}", response_model=Books, dependencies=[role_checker])
-async def get_a_book(book_id: str, session: AsyncSession = Depends(get_session), user_details= Depends(access_token_bearer)) -> dict:
+async def get_a_book(book_id: str, session: AsyncSession = Depends(get_session), token_details: dict = Depends(access_token_bearer)) -> dict:
     # for book in books:
     #     if book["id"] == book_id:
     #         return book
@@ -37,7 +43,7 @@ async def get_a_book(book_id: str, session: AsyncSession = Depends(get_session),
     return book
 
 @book_router.patch("/{book_id}", response_model=Books, dependencies=[role_checker])
-async def update_a_book(book_id: str, update_book: BookUpdate, session: AsyncSession = Depends(get_session), user_details= Depends(access_token_bearer)) -> dict:
+async def update_a_book(book_id: str, update_book: BookUpdate, session: AsyncSession = Depends(get_session), token_details: dict = Depends(access_token_bearer)) -> dict:
     # for book in books:
     #     if book["id"] == book_id:
     #         book['title'] = update_book.title
@@ -53,7 +59,7 @@ async def update_a_book(book_id: str, update_book: BookUpdate, session: AsyncSes
     return updated_book
 
 @book_router.delete("/{book_id}", status_code= status.HTTP_204_NO_CONTENT, dependencies=[role_checker])
-async def delete_a_book(book_id: str, session: AsyncSession = Depends(get_session), user_details= Depends(access_token_bearer)):
+async def delete_a_book(book_id: str, session: AsyncSession = Depends(get_session), token_details: dict = Depends(access_token_bearer)):
     # for book in books:
     #     if book["id"] == book_id:
     #         books.remove(book)
