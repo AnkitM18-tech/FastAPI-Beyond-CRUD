2fc5d097f1f359d35ba1d653883c5f6ad5817be9
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-16 01:14:33 +0530

    allowed hosts changed

--- bookly/src/middleware.py
+++ bookly/src/middleware.py
@@ -35,5 +35,5 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts=["*"],
+        allowed_hosts=["localhost", "127.0.0.1"],
     )
