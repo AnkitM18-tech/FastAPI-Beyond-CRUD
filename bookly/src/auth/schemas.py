fd7a89632baeed0e29427f58322598001845cebc
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-08-16 16:57:09 +0530

    email recipients schema

--- bookly/src/auth/schemas.py
+++ bookly/src/auth/schemas.py
@@ -31,3 +31,6 @@ class UserBooks(UserModel):
 class UserLogin(BaseModel):
     email: str = Field(max_length=40)
     password: str = Field(min_length=6)
+
+class Email(BaseModel):
+    email_addresses: List[str]
